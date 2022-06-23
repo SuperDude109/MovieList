0112c19359374af4a659d788ab849fe635e4ec55
superDude109 <superdude@Bobs-Mouth.localdomain>
2022-06-23 13:08:25 -0500

    level 2 complete

--- frontEnd/movie-list/src/App.js
+++ frontEnd/movie-list/src/App.js
@@ -1,21 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
-const movies = [
-  {title: 'Mean Girls'},
-  {title: 'Hackers'},
-  {title: 'The Grey'},
-  {title: 'Sunshine'},
-  {title: 'Ex Machina'},
-];
+import { useState, useEffect } from 'react';
+
+
+const movies = []
 
 function App() {
+  let [movieList,setMovieList]= useState([])
+  useEffect(() => {
+    fetch("http://localhost:3001/movies")
+    .then(response => response.json())
+    .then(data => {
+      console.log("Here is the data",data)
+      setMovieList([...data])})
+  }, []);
+
   return (
     <div className="App">
       <header>
           Movie List
       </header>
       <div>
-        <ol>{movies.map((movie,index) => {return (<li key = {index}>{movie.title}</li>)})}</ol>
+        <ol>{movieList.map((movie,index) => {return (<li key = {index}>{movie.title}</li>)})}</ol>
       </div>
     </div>
   );
